1b3956c2e3082bf869bb29323409547420200858
k-iseri <water_dropwort@hotmail.com>
2015-06-30 02:05:40 +0900

    Update sketch_june12pantilt.ino

--- sketchbook/sketch_june12pantilt/sketch_june12pantilt.ino
+++ sketchbook/sketch_june12pantilt/sketch_june12pantilt.ino
@@ -1,73 +1,57 @@
-#include <Servo.h> 
-int analogPin = A0;
+#include <Servo.h>
+#include <string.h>
+#include <pthread.h>
+
 int servoPin1 = 8;
 int servoPin2 = 9;
-int ledPin = 13;
-int val = 0;
 Servo myservo1;
 Servo myservo2;
-String strdeg = "";
-    
-void setup() {
 
-    myservo1.attach(servoPin1);
-    myservo2.attach(servoPin2);
-    pinMode(analogPin, INPUT);
-    pinMode(ledPin, OUTPUT);
-    Serial.begin(9600);	// 9600bpsでポートを開く
+void setup() {
+    pthread_t thread;
+    pthread_create(&thread, NULL, loop_thread, NULL);
+    Serial.begin(9600);    // 9600bpsでポートを開く
+}
+
+void *loop_thread(void *param) {
+    while(1) {
+        /* シリアル通信から1行とりだす */
+        String strSer = "";
+        while (Serial.available() > 0) {
+            char c = Serial.read();
+            if (c == '\n') break;
+            strSer += c;
+        }
+
+        /* シリアル命令を解析 */
+
+        char opName[20];
+        int opValue;
+        sscanf(strSer.data(), "%s:%d", opName, &opValue);
+        if (strcmp(opName, "pan") == 0) {
+            myservo1.attach(servoPin1);
+            myservo1.write(opValue);
+        } else if (strcmp(opName, "tilt") == 0) {
+            myservo2.attach(servoPin2);
+            myservo2.write(opValue);
+        } else if (strcmp(opName, "nod") == 0) {
+            for (int i = 0; i < opValue; i++) {
+                myservo1.attach(servoPin1);
+                myservo2.attach(servoPin2);
+                myservo1.write(90);
+                myservo2.write(30);
+                delay(1000);
+                myservo2.write(120);
+                delay(1000);
+            }
+            myservo2.write(90);
+        } else if (strcmp(opName, "detach") == 0) {
+            myservo1.detach();
+            myservo2.detach();
+        }
+        return NULL;
+    }
 }
 
 void loop() {
-        myservo1.write(90);
-        myservo2.write(90);
-            delay(500);
-        myservo1.write(0);
-        myservo2.write(90);
-            delay(500);
-        myservo1.write(180);
-        myservo2.write(90);
-            delay(500);  
-        myservo1.write(0);
-        myservo2.write(90);
-            delay(500);
-        myservo1.write(90);
-        myservo2.write(90);
-            myservo1.detach();
-            myservo2.detach();
-            delay(500);  
-        myservo1.write(90);
-        myservo2.write(30);
-            delay(1000);    
-        myservo1.write(90);
-        myservo2.write(120);
-            delay(1000);            
-        myservo1.write(90);
-        myservo2.write(90);
-            delay(1000);
-        myservo1.write(90);
-        myservo2.write(90);
-            delay(100000000);            
-/*    val = analogRead(analogPin);
-    char valstr[32];
-    sprintf(valstr, "%d", val);
-    Serial.println(valstr);
-
-    while (Serial.available() > 0) {
-      char c = Serial.read();
-      strdeg += c;
-    }
-    String senddeg = strdeg;
-    strdeg = "";
-    if (senddeg != "") {
-      Serial.println(senddeg);
-      myservo.write(senddeg.toInt());
-      if (senddeg.toInt() > 100) {
-        digitalWrite(ledPin, HIGH);
-      } else {
-        digitalWrite(ledPin, LOW);
-      }
-    }
-    */
-
-    
 }
