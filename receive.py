65c657118f142523f53576d1a7220e866d7d4ed1
yutayaguchi <yutayaguchi@users.noreply.github.com>
2015-07-01 03:02:45 +0900

    Update receive.py

--- receive.py
+++ receive.py
@@ -12,7 +12,16 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         if self.path.endswith('favicon.ico'):
             return
 
-        elif self.path.endswith('/'):        
+        elif self.path.endswith('ajax.json'): 
+            html = """
+{"sensor1": "1", "sensor2": "0"}
+"""
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(html)
+            return
+
+        elif self.path.endswith('/') or self.path.endswith('index.html'):        
             self.send_response(200)
             self.end_headers()
             f = open("monitor.html")
@@ -20,30 +29,34 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.wfile.write(response_body)
             return
 
-        elif self.path.endswith('ajax.json'): 
-            html = """
-{"sensor1": "1", "sensor2": "0"}
-"""
-            self.wfile.write(html)
-        elif self.path.endswith('post'):
-            if os.environ['REQUEST_METHOD'] == 'POST':
-                data = cgi.FieldStorage()
-                if data['pan'].value.isdigit():
-                    pandeg = int(data['pan'].value)
-                    if pandeg >= 0 and pandeg <= 180:
-                        ser = serial.Serial('/dev/ttyACM0', timeout=5.0)
-                        ser.write("pan:" + pandeg)
-                        ser.close()
-                if data['tilt'].value.isdigit():
-                    tiltdeg = int(data['tilt'].value)
-                    if tiltdeg >= 0 and tiltdeg <= 180:
-                        ser = serial.Serial('/dev/ttyACM0', timeout=5.0)
-                        ser.write("tilt:" + tiltdeg)
-                        ser.close()
-                if data['talk'].value
-                    os.system('./aquestalkpi/AquesTalkPi "' + data['talk'].value + '" | aplay&')
+    def do_POST(self):
+        if self.path.endswith('post'):
+            form = cgi.FieldStorage(
+            fp=self.rfile, 
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
+            
+            if form.has_key('pan') and (form['pan'].value).isdigit():
+                pandeg = int(form['pan'].value)
+                if pandeg >= 0 and pandeg <= 180:
+                    ser = serial.Serial('/dev/ttyACM0', timeout=5.0)
+                    ser.write('pan:' + form['pan'].value)
+                    ser.close()
+            if form.has_key('tilt') and (form['tilt'].value).isdigit():
+                tiltdeg = int(form['tilt'].value)
+                if tiltdeg >= 0 and tiltdeg <= 180:
+                    ser = serial.Serial('/dev/ttyACM0', timeout=5.0)
+                    ser.write("tilt:" + form['tilt'].value)
+                    ser.close()
+            if form.has_key('talk'):
+                os.system('./aquestalkpi/AquesTalkPi "' + form['talk'].value + '" | aplay&')
+            self.send_response(200)
+            self.end_headers()
             self.wfile.write("ok")
-            return
-server_address = ('', 8000)
-httpd = BaseHTTPServer.HTTPServer(server_address, MyHandler)
-httpd.serve_forever()
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = BaseHTTPServer.HTTPServer(server_address, MyHandler)
+    httpd.serve_forever()
